be15331c8271bf11524de05b591583be426354c0
Dustin Flynn <flynnd297@gmail.com>
2021-10-27 10:42:21 -0500

    Installed axios, materialUI, and jwtDecode.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import './App.css';
+import React, { Component } from 'react';
+import jwtDecode from 'jwt-decode';
+import axios from 'axios';
+import { Redirect, Switch, Route } from 'react-router';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          
+        </header>
+      </div>
+    );
+}
 }
 
 export default App;
